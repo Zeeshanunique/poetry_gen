b1efe9d15c8e28584eabd93fbf33fb88242adc41
Zeeshanunique <zeeshanunique2619@email.com>
2024-04-27 16:59:20 +0530

    Added time

--- poetry.py
+++ poetry.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.llms import OpenAI
+# from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAI
 
@@ -13,11 +13,24 @@ def main():
     city = st.text_input("Enter a city:")
     pollution_rate = st.number_input("Enter the pollution rate (0-100):", min_value=0, max_value=100)
     length = st.number_input("Enter the desired poem length (number of lines):", min_value=1)
+    date = st.date_input("Select a date:")
 
     # Generate poetry button
     if st.button("Generate Poetry"):
         if city and 0 <= pollution_rate <= 100 and length > 0:
-            prompt = f"Craft a lyrical and evocative poem about {city}, a city with a pollution rate of {pollution_rate}%. The poem should be {length} lines long, painting a vivid picture of the city's atmosphere, its people, and the impact of pollution on their lives. Use powerful imagery and metaphors to convey the emotions and experiences of the city's beauty and culture, while maintaining a rhythmic and melodic flow throughout the poem. The tone of the poem should be pleasing if the pollution rate is less than 40, medium if it's between 40 and 60, and more critical and rough if it's above 60. Don't generate negative poem. Don't mention the word smog."            # llm = OpenAI(model_name="text-bison@001", temperature=0.7)
+            prompt = f"""Craft a lyrical and evocative poem about {city}, a city with a pollution rate of {pollution_rate}% on {date.strftime('%B %d, %Y')} The poem should be {length} lines long, painting a vivid picture of the city's atmosphere, its people, and the impact of pollution on their lives. 
+            Use powerful imagery and metaphors to convey the emotions and experiences of the city's beauty and culture, while maintaining a rhythmic and melodic flow throughout the poem. 
+            The tone of the poem should be pleasing if the pollution rate is less than 40, medium if it's between 40 and 60, and more critical and rough if it's above 60. Don't generate negative poem. 
+            Don't mention the word smog.
+            In poetry mention the city history and culture.
+            Take the reference of {city} city and pollution rate {pollution_rate}%.
+
+
+            
+            
+            """
+            
+                       # llm = OpenAI(model_name="text-bison@001", temperature=0.7)
             llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
             poem = llm.invoke(prompt)
             st.subheader("Generated Poem:")
